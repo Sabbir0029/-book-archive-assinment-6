93aae723e0515988b1d5d5f1fc4321bc1e16f21d
sabbir0029 <mdsabbirhossain0029@gmail.com>
2021-09-02 18:30:27 +0600

    add

--- js/app.js
+++ js/app.js
@@ -3,43 +3,44 @@ const button = () =>{
   const inputId = document.getElementById('inputId');
   const inputValue = inputId.value;
   inputId.value = '';
-  if(inputValue === ''){
-    // Error message
-    const error = document.getElementById('error');
-    const div = document.createElement('div');
-    div.innerHTML=`<p>No Results Found</p>`;
-    error.append(div);
-  }
-  else{
     const url =` https://openlibrary.org/search.json?q=${inputValue}`;
     fetch(url)
     .then(res => res.json())
     .then(data => display(data.docs));
-  }
 }
 const display = (books) =>{
   // Search Result length
-  searchResult.innerHTML =`<h2>search Result ${books.length}</h2>`;
-  books.forEach(book => {
-    console.log(book);
-    const dispalyId = document.getElementById('dispalyId');
-    // Create Element
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML =`
-    <div class="card">
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
+  searchResult.innerHTML =`<h2 class='fs-3 fw-bold text-success'>Search results ${books.length} books</h2>`;
+  dispalyId.innerHTML = '';
+  if(books.length === 0){
+        // Error message
+        const error = document.getElementById('error');
+        const div = document.createElement('div');
+        div.innerHTML=`<p>No Results Found</p>`;
+        error.append(div);
+  }
+  else{
+    books.forEach(book => {
+      const dispalyId = document.getElementById('dispalyId');
+      // Create Element
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML =`
+      <div class="card">
+          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Author Name : ${book.author_name}</li>
+            <li class="list-group-item">First Publish Year : ${book.first_publish_year}</li>
+            <li class="list-group-item">publisher : ${book.publisher}</li>
+          </ul>
         </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Author Name : ${book.author_name}</li>
-          <li class="list-group-item">First Publish Year : ${book.first_publish_year}</li>
-          <li class="list-group-item">publisher : ${book.publisher}</li>
-        </ul>
-      </div>
-    `;
-    // Append Child
-    dispalyId.appendChild(div);
-  });
+      `;
+      // Append Child
+      dispalyId.appendChild(div);
+      error.innerHTML ='';
+    });
+  }
 }
